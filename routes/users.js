eb2715c356851309bf1e9c4945c7f7716afb3543
Kévin Bah <kevinbah007@gmail.com>
2024-02-06 21:00:05 +0100

    correction signup/signin routes

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+// KB a créé les routes signup et signin
+
 var express = require('express');
 var router = express.Router();
 
@@ -8,17 +10,18 @@ const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
 router.post('/signup', (req, res) => {
-  const token = uid2(32);
-  const hash = bcrypt.hashSync(req.body.password, 10);
-
-	if (!checkBody(req.body, ['username','email', 'password'])) {
+  // CheckBody functions checks that there are fields username, email and passwords in req.body
+  if (!checkBody(req.body, ['username','email', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
 
   // Check if the user has not already been registered
-  User.findOne({ username: req.body.email }).then(data => {
+  User.findOne({ email: req.body.email }).then(data => {
     if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const token = uid2(32);
+
       const newUser = new User({
         username: req.body.username,
         email: req.body.email,
@@ -26,8 +29,8 @@ router.post('/signup', (req, res) => {
         token: token,
       });
 
-      newUser.save().then(() => {
-        res.json({ result: true, token: token });
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
       });
     } else {
       // User already exists in database
@@ -39,17 +42,16 @@ router.post('/signup', (req, res) => {
 router.post('/signin', (req, res) => {
   if (!checkBody(req.body, ['email', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
-    return;cd
+    return;
   }
 
-  User.findOne({ username: req.body.email }).then(data => {
+  User.findOne({ email: req.body.email }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
-      res.json({ result: false });
+      res.json({ result: false, error: 'User not found or wrong password' });
     }
   });
 });
-
 
 module.exports = router;
