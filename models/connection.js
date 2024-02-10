c61ae6498fd22780e5a2f15beb7ff0ab9e47745c
Kévin Bah <kevinbah007@gmail.com>
2024-02-10 10:43:07 +0100

    connection en string en dur dans connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://canniconnect:FullSt4ck@cluster0.dgcnj19.mongodb.net/caniconnect';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
 
-// URL du backend : https://backend-lyart-mu.vercel.app/
+// URL du backend : https://backend-one-nu-35.vercel.app/
