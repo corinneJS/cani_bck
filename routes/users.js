6778db977359b52eafceda579182a66de6df7c1c
Kévin Bah <kevinbah007@gmail.com>
2024-02-12 18:59:51 +0100

    suppression des commentaires autour de checkbody dans route users/signup

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.get('/hello', (req, res) => {
 
 router.post('/signup', (req, res) => {
   // CheckBody functions checks that there are fields username, email and passwords in req.body
-  /* if (!checkBody(req.body, [
+  if (!checkBody(req.body, [
     'username',
     'email', 
     'password', 
@@ -25,7 +25,7 @@ router.post('/signup', (req, res) => {
   ])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
-  } */
+  }
 
   const valueOfIsDogOwner = true;
   if (req.body.isDogOwner === true || req.body.isDogOwner === false){
