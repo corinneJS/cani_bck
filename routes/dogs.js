a5737920e462b641b689384a5419db6a934d102a
corinneJS <corinne.poisson.42@gmail.com>
2024-02-21 16:13:16 +0100

    Modif dogs

--- routes/dogs.js
+++ routes/dogs.js
@@ -15,6 +15,7 @@ router.get("/first_fromuser/:userID", (req, res) => {
   .then((data) => {
     console.log(data);
     if (data) {
+      console.log(data)
       res.json({ result: true, dog: data });
       return;
     }
@@ -25,21 +26,21 @@ router.get("/first_fromuser/:userID", (req, res) => {
 })
 
 
-/* router.post("/create", (req,res) => {
+router.post("/create", (req,res) => {
 const newDog = new Dog({
-  dogName: "Toupie",
+  dogName: "Olea",
   description: "",
   birthdate: new Date(),
   isFemale: false,
   isSterilized: false,
-  traitID: [{ type: mongoose.Schema.Types.ObjectId, ref: "traits" }],
-  activityID: [{ type: mongoose.Schema.Types.ObjectId, ref: "activities" }], 
+  traitID: ['65cd466235cbacf79a033e62','65cd466235cbacf79a033e64'],
+  activityID: [], 
   dateCreated: new Date(),
   dateModified: new Date(),
-  dogPhotos: [dogPhotoSchema], 
+  dogPhotos: [{}], 
   userID: '65cd15729a02592f4d2c8810',
-  reedID: { type: mongoose.Schema.Types.ObjectId, ref: "breeds" },})
+  breedID: '65cfc64f91cf1c81a76e0b9b',})
 newDog.save();
-}); */
+}); 
 
 module.exports = router;
